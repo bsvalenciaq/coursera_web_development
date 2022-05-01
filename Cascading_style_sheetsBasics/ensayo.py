7d6f62a980562b6909606bf63097cc4712755c5d
Brayan <bsvalenciaq@unal.edu.co>
2022-04-30 19:09:39 -0500

    separating folders

--- Cascading_style_sheetsBasics/ensayo.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def l2_dist(a, b):
-    result = ((a - b) * (a - b)).np.sum()
-    result = result ** 0.5
-    return result 
-
-l2_dist(np.reshape(a, (20 * 20)), np.reshape(b, (20 * 20, 1)))
